856ebd8ee0a05a5f2277db53963300e375c5d440
istiakrifti <istiakrifti101@gmail.com>
2024-03-11 15:26:19 +0600

    new

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import './App.css';
+import NewWindow from './NewWindow';
+import Home from './Home';
+
+const App = () => {
+  const [password,setPassword] = useState('');
+
+  return (
+    
+    <Router>
+        <Routes>
+        <Route exact path="/" element={<Home setPassword={setPassword}/>}/>
+        <Route path="/new-page/:password" element={<NewWindow pass={password} setPassword={setPassword}/>} />
+        </Routes>
+    </Router>
+    
+  );
+};
+
+export default App;
