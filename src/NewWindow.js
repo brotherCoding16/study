b3c320118613ebb1de16565b8b4bb6d3f45ece0a
brotherCoding16 <istiakrifti101@gmail.com>
2024-03-17 21:49:00 +0600

    commit24

--- src/NewWindow.js
+++ src/NewWindow.js
@@ -1,6 +1,7 @@
 import ExamButton from './ExamButton';
 import React, { useState,useEffect } from 'react';
-
+import "./home.css";
+import "./das.css";
 // import { useNavigate } from 'react-router-dom';
 
 
@@ -34,7 +35,7 @@ const NewWindow = (props) => {
 
   const handleJoinMeet = () => {
     //console.log(props.pass);
-    if(props.pass==="mahdir10") window.open("https://meet.google.com/gqh-urnz-ixd", '_blank');
+    if(props.pass==="mahdir10") window.open("https://meet.google.com/syz-vfog-was", '_blank');
     if(props.pass==="afiqrcs15") window.open("https://meet.google.com/fud-vqam-vpx", '_blank');
     if(props.pass==="esha10") window.open("https://meet.google.com/igs-jkgu-zkb", '_blank');
   };
