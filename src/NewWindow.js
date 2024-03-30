c59961b62909b8abc69771e231ad4fa186fb7fd8
brotherCoding16 <istiakrifti101@gmail.com>
2024-03-30 21:17:36 +0600

    new6

--- src/NewWindow.js
+++ src/NewWindow.js
@@ -38,7 +38,7 @@ const NewWindow = (props) => {
     if(props.pass==="mahdir10") window.open("https://meet.google.com/hyi-okwm-usg", '_blank');
     if(props.pass==="afiqrcs15") window.open("https://meet.google.com/fud-vqam-vpx", '_blank');
     if(props.pass==="esha10") window.open("https://meet.google.com/igs-jkgu-zkb", '_blank');
-    if(props.pass==="afsana10") window.open("https://meet.google.com/ovh-zqcc-sns", '_blank');
+    if(props.pass==="afsana10") window.open("https://meet.google.com/qse-prwx-mwb", '_blank');
   };
 
   const handleContentSelect = (content) => {
