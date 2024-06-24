9f171fd9e9771972d7160ddb1b78b68cf4e0411c
brotherCoding16 <istiakrifti101@gmail.com>
2024-06-24 10:59:30 +0600

    new commit10

--- src/NewWindow.js
+++ src/NewWindow.js
@@ -40,7 +40,7 @@ const NewWindow = (props) => {
     //console.log(props.pass);
     if(props.pass==="mahdir10") window.open("https://meet.google.com/dbp-pfmf-kas", '_blank');
     if(props.pass==="afiqrcs15") window.open("https://meet.google.com/fud-vqam-vpx", '_blank');
-    if(props.pass==="esha10") window.open("https://meet.google.com/wkb-jovi-paz", '_blank');
+    if(props.pass==="esha10") window.open("https://meet.google.com/oas-xpop-muv", '_blank');
     if(props.pass==="afsana10") window.open("https://meet.google.com/yca-znqn-tzq", '_blank');
     if(props.pass==="abir10") window.open("https://meet.google.com/ond-winx-fnd", '_blank');
   };
